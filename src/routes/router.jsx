dd7ab50815d2a3f42113199c5ea1c7329e0e5b96
junsu <junsu.kim@danbplus.com>
2025-06-26 09:16:22 +0900

    커밋?

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -4,7 +4,7 @@ import Layout from "../layout/Layout";
 import Blog from "../pages/Blog";
 
 const { createBrowserRouter } = require("react-router-dom");
- 
+
 const router = createBrowserRouter([
     {
         path: "/",
