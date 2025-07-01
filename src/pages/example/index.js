051865f07c583d7e58172319c16a9f9feebbad28
junsu <junsu.kim@danbplus.com>
2025-07-01 13:35:49 +0900

    20250627 KJS - 폴더명 수정

--- src/pages/example/index.js
+++ src/pages/example/index.js
@@ -1,14 +1,14 @@
 import Blog from "./blog/Blog";
-import UseState from "./hook-example/useState/UseState";
-import UseEffect from "./hook-example/useEffect/UseEffect";
-import UseRef from "./hook-example/useRef/UseRef";
+import UseState from "./hook-example/01.useState/UseState";
+import UseEffect from "./hook-example/02.useEffect/UseEffect";
+import UseRef from "./hook-example/03.useRef/UseRef";
 import UseContext from "./hook-example/useContext/context/UseContext";
 import PropsDrilling from "./hook-example/useContext/drilling/PropsDrilling";
-import UseMemo from "./hook-example/useMemo/UseMemo";
-import UseCallback from "./hook-example/useCallback/UseCallback";
-import UseReducer from "./hook-example/useReducer/UseReducer";
-import ReactMemo from "./hook-example/reactMemo/ReactMemo";
-import CustomHook from "./hook-example/customHook/CustomHook";
+import UseMemo from "./hook-example/05.useMemo/UseMemo";
+import UseCallback from "./hook-example/06.useCallback/UseCallback";
+import UseReducer from "./hook-example/07.useReducer/UseReducer";
+import ReactMemo from "./hook-example/08.reactMemo/ReactMemo";
+import CustomHook from "./hook-example/09.customHook/CustomHook";
 
 export {
     Blog,
